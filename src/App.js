74214faa4bafebc95f8ec3b5620d83c4cf04a220
FaizanKhanDev <faizan480khan@gmail.com>
2024-04-29 10:37:00 -0700

    ADDED: myName

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import Header from './Header.js';
-import Footer from './Footer.jsx';
-
+// import Header from './Header.js';
+// import Footer from './Footer.jsx';
+import { useState } from 'react';
 function App() {
-  let myObject = {
-    header:"This is Header",
-    headerSubTitle:"THis is Sub title",
-    footer:"This is Footer"
 
-  }
+  const [myName, setMyName] = useState('');
+  const [myRollNumber, setMyRollNumber] = useState(0);
+
+
+  // const addValue = (event) => {
+  //     setMyName(event.target.value)
+
+  //     console.log("myName", myName);
+  // }
+
   return (
-      <div>
-        <Header 
-          title={myObject.header} 
-          subTitle={myObject.headerSubTitle}
-          >
-        </Header>
+    <div>
 
-        
-        <Footer 
-          title={myObject.footer}>
-        </Footer>
-      </div>    
+      {/* onChange={addValue} */}
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={myName}
+        onChange={(event) => setMyName(event.target.value)}
+      />
+      <br />
+
+      <input
+        type="text"
+        value={myRollNumber}
+        onChange={(event) => setMyRollNumber(event.target.value)}
+      />
+      <br />
+    Name: {myName} <br />
+    Roll Number:  {myRollNumber}
+
+
+
+
+      {/* <Header
+        title={myObject.header.value}
+        subTitle={myObject.headerSubTitle}
+      >
+      </Header>
+      <Footer
+        title={myObject.footer}>
+      </Footer> */}
+    </div>
   );
 }
 
